0163263f4ed8751b38617457fc2d3682f76b12b4
just4developments <just4developments@gmail.com>
2016-11-17 10:30:13 +0700

    U: Update port

--- src/appconfig.js
+++ src/appconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    listen: 8000,
-    staticUrl: 'http://localhost:8000',
+    listen: 8001,
+    staticUrl: 'http://localhost:8001',
     db: {
         url: 'mongodb://localhost:27017/nanacloset01'
     }
