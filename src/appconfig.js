288a3eb3b524d3866fb28cc43e4ed7a551b1f8ae
just4developments <just4developments@gmail.com>
2016-11-23 15:32:41 +0700

    U: UPdate image size

--- src/appconfig.js
+++ src/appconfig.js
@@ -3,5 +3,14 @@ module.exports = {
     staticUrl: 'http://localhost:9000',
     db: {
         url: 'mongodb://localhost:27017/nanacloset01'
+    },
+    app: {
+        imageResize: {
+			product: [
+                {w: 110, h: 55, ext: 'small'}, // in menu
+                {w: 100, h: 100, ext: 'middle'}, // details
+                {w: 180, h: 180, ext: 'large'} // update
+			]
+        }
     }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,31 +1,90 @@
 let path = require('path');
 let restify = require('restify');
 let fs = require('fs');
+let async = require('async');
 
 /************************************
  ** CLASS HELPER
  ** 
  *************************************/
 
-module.exports = {
+exports = module.exports = {
+    uuid: () => {
+        let uuid = require('node-uuid');
+        return uuid.v4();
+    },
     has: (params) => {
         return params !== undefined && params !== null;
     },
     jsonHandler: (config) => {
         return restify.bodyParser(config);
     },
+    resizeImage: (file, sizes) => {
+        return new Promise((resolve, reject) => {
+            let Jimp = require('jimp');            
+            let rstasks = [];
+            sizes.forEach((size) => {
+                rstasks.push(((file, size, cb) => {
+                    let fileout = file.substr(0, file.lastIndexOf('.')) + '.' + size.ext + file.substr(file.lastIndexOf('.'));
+                    Jimp.read(file, function (err, image) {
+                        if (err) return fcError(err);
+                        var w, h;
+                        if (image.bitmap.width > image.bitmap.height) {
+                            w = image.bitmap.width * size.h / image.bitmap.height;
+                            h = size.h;
+                        } else {
+                            h = image.bitmap.height * size.w / image.bitmap.width;
+                            w = size.w;
+                        }
+                        var x = 0; //Math.abs((w-size.w)/2)*-1;
+                        var y = 0; //Math.abs((h-size.h)/2)*-1;
+                        image.resize(w, h)
+                            .crop(x, y, size.w, size.h)
+                            .quality(100)
+                            .write(fileout, (params) => {
+                                cb(null, fileout);
+                            });
+                    });
+                }).bind(null, file, size));
+            });
+            async.parallel(rstasks, (err, results) => {
+                if (err) return reject(err);
+                resolve(results);
+            });
+        });
+    },
     fileUploadHandler: (config) => {
         let defaultConfig = {
-            mapFiles: true,
+            uploadDir: "assets/upload/",
+            httpPath: "/upload/${filename}",
+            resize: undefined,
+            mapFiles: false,
             mapParams: false,
-            keepExtensions: true,
-            multiples: false
+            keepExtensions: false,
+            multiples: false,
+            multipartFileHandler: (part, req) => {
+                    let datas = new Buffer(0);
+                    let filename = exports.uuid() + (part.filename.indexOf('.') !== -1 ? part.filename.substr(part.filename.lastIndexOf('.')) : '');
+                    let fileout = path.join(defaultConfig.uploadDir, filename);
+                    part.on('data', function (data) {
+                        datas = Buffer.concat([datas, data]);
+                    });
+                    part.on('end', () => {
+                        fs.writeFileSync(fileout, datas);
+                        if (!req.file) req.file = {};
+                        if (!req.file[part.name]) req.file[part.name] = defaultConfig.multiples ? [] : {};
+                        if (req.file[part.name] instanceof Array) req.file[part.name].push(defaultConfig.httpPath.replace('${filename}', filename));
+                        else req.file[part.name] = defaultConfig.httpPath.replace('${filename}', filename);
+                        if (defaultConfig.resize) {
+                            exports.resizeImage(fileout, defaultConfig.resize).catch((err) => {
+                                console.error('RESIZE_IMAGE', err);
+                            });
+                        }
+                    });
+                }
                 // multipartHandler: function (part) {
                 //     part.on('data', function (data) { });
                 // },
-                // multipartFileHandler: function (part) {
-                //     part.on('data', function (data) { });
-                // }
         };
         if (config instanceof Object) {
             defaultConfig = Object.assign(defaultConfig, config);
@@ -35,10 +94,10 @@ module.exports = {
         }
         return restify.bodyParser(defaultConfig);
     },
-    getAbsoluteUpload: (files) => {
+    getAbsoluteUpload: (files, rootPath) => {
         if (!files) return files;
         this.getPath = (f) => {
-            return path.join(__dirname, '..', 'assets', f);
+            return path.join(rootPath, f.substr(f.lastIndexOf('/') + 1));
         };
         if (!(files instanceof Array)) {
             return this.getPath(files);
@@ -50,19 +109,24 @@ module.exports = {
         return rs;
 
     },
-    deleteFile: (files) => {
+    deleteFile: (files, sizes) => {
         if (!files) return;
-        this.remove = (f) => {
+        let remove = (f, sizes) => {
             try {
                 fs.statSync(f);
                 fs.unlinkSync(f);
             } catch (e) { /*File was removed before that*/ }
+            if(sizes){
+                sizes.forEach((s) => {
+                    remove(f.substr(0, f.lastIndexOf('.')) + '.' + s.ext + f.substr(f.lastIndexOf('.')));
+                });
+            }
         };
         if (!(files instanceof Array)) {
-            return this.remove(files);
+            return remove(files, sizes);
         }
         for (var f of files) {
-            this.remove(f);
+            remove(f, sizes);
         }
     },
     getPathUpload: (file, returnPath, multiples) => {
