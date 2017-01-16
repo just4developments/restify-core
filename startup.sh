027114c0bce0b3f9d6b312e841ed8dd1145ff59e
thanhdt <thanhdt@viosoft.com>
2017-01-16 13:00:33 +0700

    U: Change name in pm2

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
-pm2 start index.js --node-args="--expose-gc --harmony-async-await"
+pm2 start index.js --node-args="--expose-gc --harmony-async-await" --name "validium-auth"
